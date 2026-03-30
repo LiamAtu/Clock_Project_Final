df8da96fcbbc29f336fb11049713607023a8766e
Liam <g00435609@atu.ie>
2026-03-30 15:13:03 +0100

    Started on logic for the rotation of the hands according to the current time

---
+++ logic.js
@@ -0,0 +1,15 @@
+var second = 0;
+var minute=0;
+var hour = 0;
+var d = new Date();
+
+setInterval(
+
+    function(){
+        d = Date();
+        second = d.getSeconds() * 6;
+        minute = d.getMinutes();
+        hour = d.getHours();
+        document.getElementById("second-hand").style.transform = "rotate(" + second + "deg)";
+    }, 1000
+);
